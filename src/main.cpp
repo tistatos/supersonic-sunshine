267ae1774d65c522ab771bdf34ad60cbb7917b55
tistatos <tistatos@gmail.com>
2016-10-31 17:12:46 +0100

    nano gui working :wine_glass:

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,27 @@
 #include <GLFW/glfw3.h>
 
 
+#include <nanogui/nanogui.h>
 #include <iostream>
+
+using namespace nanogui;
+
+enum test_enum {
+    Item1 = 0,
+    Item2,
+    Item3
+};
+
+bool bvar = true;
+int ivar = 12345678;
+double dvar = 3.1415926;
+float fvar = (float)dvar;
+std::string strval = "A string";
+test_enum enumval = Item2;
+Color colval(0.5f, 0.5f, 0.7f, 1.f);
+
+Screen *screen = nullptr;
+
 
 void errorcb(int code, const char* msg){
 	std::cout << "code: " << code << " msg: " << msg ; 
@@ -49,10 +69,40 @@ int main(int argc, char *argv[]) {
 	glewExperimental = GL_TRUE;
 	glewInit();
 
+    // Create a nanogui screen and pass the glfw pointer to initialize
+    screen = new Screen();
+    screen->initialize(mWindow, true);
+ bool enabled = true;
+    FormHelper *gui = new FormHelper(screen);
+    ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+    gui->addGroup("Basic types");
+    gui->addVariable("bool", bvar)->setTooltip("Test tooltip.");
+    gui->addVariable("string", strval);
+
+    gui->addGroup("Validating fields");
+    gui->addVariable("int", ivar)->setSpinnable(true);
+    gui->addVariable("float", fvar)->setTooltip("Test.");
+    gui->addVariable("double", dvar)->setSpinnable(true);
+
+    gui->addGroup("Complex types");
+    gui->addVariable("Enumeration", enumval, enabled)->setItems({ "Item 1", "Item 2", "Item 3" });
+    gui->addVariable("Color", colval);
+
+    gui->addGroup("Other widgets");
+    gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; })->setTooltip("Testing a much longer tooltip, that will wrap around to new lines multiple times.");;
+
+    screen->setVisible(true);
+    screen->performLayout();
+    nanoguiWindow->center();
+
+
   while (!glfwWindowShouldClose(mWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        screen->drawContents();
+        screen->drawWidgets();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(mWindow);
