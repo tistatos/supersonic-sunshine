6545f3979014daec6161dcb58aac5001a114d7c4
Porraryd <pontusorraryd@gmail.com>
2016-10-31 16:24:05 +0100

    osx fix :beers:

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,20 @@
 */
 
 #include <GL/glew.h>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 #include <GLFW/glfw3.h>
+
+
+#include <iostream>
+
+void errorcb(int code, const char* msg){
+	std::cout << "code: " << code << " msg: " << msg ; 
+}
 
 int main(int argc, char *argv[]) {
   GLFWmonitor* mMonitor;
@@ -18,11 +30,17 @@ int main(int argc, char *argv[]) {
   const GLFWvidmode* vidmode;
   mMonitor = glfwGetPrimaryMonitor();
   vidmode = glfwGetVideoMode(mMonitor);
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
+	glfwSetErrorCallback(errorcb);	
 	mWindow = glfwCreateWindow(vidmode->width/2, vidmode->height/2, "snopp", NULL, NULL);
 	if (!mWindow) {
+		std::cout << "error creating window";
 		glfwTerminate();
 		return false;
 	}
@@ -31,11 +49,17 @@ int main(int argc, char *argv[]) {
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-  while(true) {
-	glClearColor(0.3f, 0.3f, 1.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glfwSwapBuffers(mWindow);
-	}
+  while (!glfwWindowShouldClose(mWindow))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(mWindow);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 	glfwTerminate();
   return 0;
 }
