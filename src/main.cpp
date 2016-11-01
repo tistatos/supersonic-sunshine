b81afbb35c63f0a780967d3241e8a4f7504e66f2
tistatos <tistatos@gmail.com>
2016-11-01 08:50:02 +0100

    nano gui creation moved to function

--- src/main.cpp
+++ src/main.cpp
@@ -19,96 +19,121 @@
 
 using namespace nanogui;
 
-enum test_enum {
-    Item1 = 0,
-    Item2,
-    Item3
-};
+Screen* createGui(GLFWwindow* window) {
+  // Create a nanogui screen and pass the glfw pointer to initialize
+  screen = new Screen();
+  screen->initialize(window, true);
+  bool enabled = true;
 
-bool bvar = true;
-int ivar = 12345678;
-double dvar = 3.1415926;
-float fvar = (float)dvar;
-std::string strval = "A string";
-test_enum enumval = Item2;
-Color colval(0.5f, 0.5f, 0.7f, 1.f);
+  bool bvar = true;
+  int ivar = 12345678;
+  double dvar = 3.1415926;
+  float fvar = (float)dvar;
+  std::string strval = "A string";
+  Color colval(0.5f, 0.5f, 0.7f, 1.f);
 
-Screen *screen = nullptr;
+  FormHelper *gui = new FormHelper(screen);
+  ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+  gui->addGroup("Basic types");
+  gui->addVariable("bool", bvar)->setTooltip("Test tooltip.");
+  gui->addVariable("string", strval);
 
+  gui->addGroup("Validating fields");
+  gui->addVariable("int", ivar)->setSpinnable(true);
+  gui->addVariable("float", fvar)->setTooltip("Test.");
+  gui->addVariable("double", dvar)->setSpinnable(true);
+
+  gui->addGroup("Complex types");
+  //gui->addVariable("Enumeration", enumval, enabled)->setItems({ "Item 1", "Item 2", "Item 3" });
+  gui->addVariable("Color", colval);
+
+  gui->addGroup("Other widgets");
+  gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; })->setTooltip("Testing a much longer tooltip, that will wrap around to new lines multiple times.");;
+
+  screen->setVisible(true);
+  screen->performLayout();
+  nanoguiWindow->center();
+
+  /** Callback lambda functions **/
+  //glfwSetCursorPosCallback(window, [](GLFWwindow *, double x, double y) {
+    //screen->cursorPosCallbackEvent(x, y);
+  //});
+
+  //glfwSetMouseButtonCallback(window, [](GLFWwindow *, int button, int action, int modifiers) {
+    //screen->mouseButtonCallbackEvent(button, action, modifiers);
+  //});
+
+  //glfwSetKeyCallback(window, [](GLFWwindow *, int key, int scancode, int action, int mods) {
+    //screen->keyCallbackEvent(key, scancode, action, mods);
+  //});
+
+  //glfwSetCharCallback(window, [](GLFWwindow *, unsigned int codepoint) {
+    //screen->charCallbackEvent(codepoint);
+  //});
+
+  //glfwSetDropCallback(window, [](GLFWwindow *, int count, const char **filenames) {
+    //screen->dropCallbackEvent(count, filenames);
+  //});
+
+  //glfwSetScrollCallback(window, [](GLFWwindow *, double x, double y) {
+    //screen->scrollCallbackEvent(x, y);
+  //});
+
+  //glfwSetFramebufferSizeCallback(window, [](GLFWwindow *, int width, int height) {
+    //screen->resizeCallbackEvent(width, height);
+  //});
+
+  return screen;
+}
 
 void errorcb(int code, const char* msg){
-	std::cout << "code: " << code << " msg: " << msg ; 
+	std::cout << "code: " << code << " msg: " << msg ;
 }
 
 int main(int argc, char *argv[]) {
-  GLFWmonitor* mMonitor;
-  GLFWwindow* mWindow;
 
 	if(!glfwInit())
 		false;
 
-  const GLFWvidmode* vidmode;
-  mMonitor = glfwGetPrimaryMonitor();
-  vidmode = glfwGetVideoMode(mMonitor);
+  GLFWmonitor* mMonitor = glfwGetPrimaryMonitor();
+  const GLFWvidmode* vidmode = glfwGetVideoMode(mMonitor);
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	glfwSetErrorCallback(errorcb);
 
-	glfwSetErrorCallback(errorcb);	
-	mWindow = glfwCreateWindow(vidmode->width/2, vidmode->height/2, "snopp", NULL, NULL);
+	GLFWwindow* mWindow = glfwCreateWindow(vidmode->width/2, vidmode->height/2, "snopp", NULL, NULL);
+
 	if (!mWindow) {
 		std::cout << "error creating window";
 		glfwTerminate();
 		return false;
 	}
+
   glfwMakeContextCurrent(mWindow);
 	glfwSwapInterval(0);
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-    // Create a nanogui screen and pass the glfw pointer to initialize
-    screen = new Screen();
-    screen->initialize(mWindow, true);
- bool enabled = true;
-    FormHelper *gui = new FormHelper(screen);
-    ref<Window> nanoguiWindow = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
-    gui->addGroup("Basic types");
-    gui->addVariable("bool", bvar)->setTooltip("Test tooltip.");
-    gui->addVariable("string", strval);
+  Screen *screen = nullptr;
+  screen = createGui(mWindow);
 
-    gui->addGroup("Validating fields");
-    gui->addVariable("int", ivar)->setSpinnable(true);
-    gui->addVariable("float", fvar)->setTooltip("Test.");
-    gui->addVariable("double", dvar)->setSpinnable(true);
+  while (!glfwWindowShouldClose(mWindow)) {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    gui->addGroup("Complex types");
-    gui->addVariable("Enumeration", enumval, enabled)->setItems({ "Item 1", "Item 2", "Item 3" });
-    gui->addVariable("Color", colval);
+    /* draw nano GUI */
+    screen->drawContents();
+    screen->drawWidgets();
 
-    gui->addGroup("Other widgets");
-    gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; })->setTooltip("Testing a much longer tooltip, that will wrap around to new lines multiple times.");;
+    /* Swap front and back buffers */
+    glfwSwapBuffers(mWindow);
 
-    screen->setVisible(true);
-    screen->performLayout();
-    nanoguiWindow->center();
-
-
-  while (!glfwWindowShouldClose(mWindow))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        screen->drawContents();
-        screen->drawWidgets();
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(mWindow);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+    /* Poll for and process events */
+    glfwPollEvents();
     }
 	glfwTerminate();
   return 0;
