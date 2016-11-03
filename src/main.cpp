af46be32924ad9b83fbdd4c31b2b72f5ddbb49b8
tistatos <tistatos@gmail.com>
2016-11-03 14:22:54 +0100

    Added assimp library with simple example

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,12 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
 
 //stl
 #include <iostream>
+#include <fstream>
 
 //local
 #include "SupersonicGui.h"
@@ -27,6 +30,45 @@
 
 glm::mat4 cameraProjection;
 glm::mat4 cameraView;
+
+bool Import3DFromFile( const std::string& pFile) {
+
+	Assimp::Importer importer;
+	//check if file exists
+	std::ifstream fin(pFile.c_str());
+	if(!fin.fail()) {
+		fin.close();
+	}
+	else {
+		printf("Couldn't open file: %s\n", pFile.c_str());
+		//printf("%s\n", importer.GetErrorString());
+		return false;
+	}
+
+	const aiScene* scene;
+	scene = importer.ReadFile( pFile, NULL);
+
+	// If the import failed, report it
+	if(!scene)
+	{
+		printf("%s\n", importer.GetErrorString());
+		return false;
+	}
+
+	// Now we can access the file's contents.
+	printf("Import of scene %s succeeded.",pFile.c_str());
+
+	//aiVector3D scene_min, scene_max, scene_center;
+	//get_bounding_box(&scene_min, &scene_max);
+	//float tmp;
+	//tmp = scene_max.x-scene_min.x;
+	//tmp = scene_max.y - scene_min.y > tmp?scene_max.y - scene_min.y:tmp;
+	//tmp = scene_max.z - scene_min.z > tmp?scene_max.z - scene_min.z:tmp;
+	//scaleFactor = 1.f / tmp;
+
+	// We're done. Everything will be cleaned up by the importer destructor
+	return true;
+}
 
 void windowResizedCallback(GLFWwindow* window, int width, int height) {
 	float aspect = (float)width/height;
